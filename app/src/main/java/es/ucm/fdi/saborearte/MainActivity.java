eb814770379ed3eacf3b7fe7164f08ffa4f99740
VeForcex <bitarveronica@gmail.com>
2023-11-15 00:33:17 +0100

    Cambio de logo, cambio de la vista, botones con degradado y mas

--- app/src/main/java/es/ucm/fdi/saborearte/MainActivity.java
+++ app/src/main/java/es/ucm/fdi/saborearte/MainActivity.java
@@ -1,26 +1,26 @@
 package es.ucm.fdi.saborearte;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
-
+import android.widget.TimePicker;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView rvRecetas;
-    private List<Receta> recetas = new ArrayList<>();  // Puedes llenar esta lista como prefieras.
-    private List<String> ingredientes_disponibles = new ArrayList<>();
-    private List<String> ingredientes_bloqueados = new ArrayList<>();
-    private String tiempo_maximo;
+    private List<Receta> recetas = new ArrayList<>();
     private RecetaResultListAdapter recetaAdapter;
+    private EditText etIngredientesDisponibles;
+    private EditText etIngredientesBloqueados;
+    private String tiempo_maximo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +28,68 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         rvRecetas = findViewById(R.id.rv_recetas);
-        EditText et_ingredientes_disponibles = findViewById(R.id.et_ingredientes_disponibles);
-        String et_string_dispo = et_ingredientes_disponibles.getText().toString();
-        ingredientes_disponibles = new ArrayList<String>(Arrays.asList(et_string_dispo.split("\\s+")));
-        EditText et_ingredientes_bloqueados = findViewById(R.id.et_ingredientes_bloqueados);
-        String et_string_bloqueados = et_ingredientes_bloqueados.getText().toString();
-        ingredientes_bloqueados = new ArrayList<String>(Arrays.asList(et_string_bloqueados.split("\\s+")));
+        etIngredientesDisponibles = findViewById(R.id.et_ingredientes_disponibles);
+        etIngredientesBloqueados = findViewById(R.id.et_ingredientes_bloqueados);
         EditText et_tiempo = findViewById(R.id.et_tiempo);
         tiempo_maximo = et_tiempo.getText().toString();
+
+        et_tiempo.setOnClickListener(v -> {
+            TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this,
+                    (view, hourOfDay, minute) -> {
+                        tiempo_maximo = String.format("%02d:%02d", hourOfDay, minute);
+                        et_tiempo.setText(tiempo_maximo);
+                    }, 0, 0, true);
+            timePickerDialog.show();
+        });
+
+        setupChipsForIngredientesDisponibles();
+        setupChipsForIngredientesBloqueados();
+        setupRecyclerView();
+    }
+
+    private void setupChipsForIngredientesDisponibles() {
+        final ChipGroup chipGroupIngredientesDisponibles = findViewById(R.id.chip_group_ingredientes_disponibles);
+        etIngredientesDisponibles.setOnKeyListener((v, keyCode, event) -> {
+            if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                String nuevoIngrediente = etIngredientesDisponibles.getText().toString().trim();
+                if (!nuevoIngrediente.isEmpty()) {
+                    createChip(nuevoIngrediente, chipGroupIngredientesDisponibles, etIngredientesDisponibles, false);
+                    return true;
+                }
+            }
+            return false;
+        });
+    }
+
+    private void setupChipsForIngredientesBloqueados() {
+        final ChipGroup chipGroupIngredientesBloqueados = findViewById(R.id.chip_group_ingredientes_bloqueados);
+        etIngredientesBloqueados.setOnKeyListener((v, keyCode, event) -> {
+            if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                String nuevoIngredienteBloqueado = etIngredientesBloqueados.getText().toString().trim();
+                if (!nuevoIngredienteBloqueado.isEmpty()) {
+                    createChip(nuevoIngredienteBloqueado, chipGroupIngredientesBloqueados, etIngredientesBloqueados, true);
+                    return true;
+                }
+            }
+            return false;
+        });
+    }
+
+    private void createChip(String text, ChipGroup chipGroup, EditText editText, boolean isBloqueado) {
+        Chip chip;
+        if (isBloqueado) {
+            chip = new Chip(this, null, R.style.ChipIngredientesBloqueados);
+        } else {
+            chip = new Chip(this, null, R.style.ChipIngredientesDisponibles);
+        }
+        chip.setText(text);
+        chip.setCloseIconVisible(true);
+        chip.setOnCloseIconClickListener(v -> chipGroup.removeView(chip));
+        chipGroup.addView(chip);
+        editText.getText().clear();
+    }
+
+    private void setupRecyclerView() {
         rvRecetas.setLayoutManager(new LinearLayoutManager(this));
         recetaAdapter = new RecetaResultListAdapter(this, recetas);
         rvRecetas.setAdapter(recetaAdapter);
